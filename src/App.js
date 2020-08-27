dded1e13b9336cf2ba38dd16e2c08bb7220ae94f
Ian Lim <ianlim@Ians-MacBook-Pro.local>
2020-08-27 12:42:25 -0500

    Added google analytics

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ import Home from './components/Home/Home';
 import Projects from './components/Projects/Projects';
 import Bookshelf from './components/Bookshelf/Bookshelf';
 
+ReactGA.initialize('UA-176552765-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 class App extends React.Component {
   constructor(props) {
     super(props);
